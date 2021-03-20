2049360351b6d748d40fd04779c23fa1d007c3d2
Roger Azevedo <roger@Rogers-MBP.lan>
2021-03-20 18:26:54 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Meu github aws test 2!'));
+app.get('/', (req, res) => res.send('Love you,   Meu github aws test 2!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
